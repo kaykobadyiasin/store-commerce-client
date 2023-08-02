edd1dcda505a4e0e17db5f5b85ac48557d99a3f1
unknown <kaykobadyiasin@gmail.com.com>
2023-08-03 03:28:50 +0600

    product image show done

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { useState } from 'react';
 import { FaMicrosoft, FaTableList } from "react-icons/fa6";
@@ -34,6 +34,14 @@ const Products = () => {
         { value: 'dell', label: 'Dell' }
     ]
 
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('electronicProducts.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
 
     return (
         <div className='min-h-screen my-5 mx-5'>
@@ -53,20 +61,15 @@ const Products = () => {
                         </div>
                     </div>
                     <div className='hero-content lg:gap-40 justify-between'>
-                        <div className='shrink-0 w-32'>
-                            <h3>Total Product 0</h3>
+                        <div className='shrink-0 w-44'>
+                            <h3 className='text-xl'>Total Product {products.length}</h3>
                         </div>
                         <div className='lg:flex-none w-48 lg:z-50'>
                             <Select className="w-full" options={options} />
-                            {/* <select className="select select-bordered w-full max-w-xs">
-                                <option disabled selected>Who shot first?</option>
-                                <option>Han Solo</option>
-                                <option>Greedo</option>
-                            </select> */}
                         </div>
                     </div>
                 </div>
-                <div className={displayType ? 'lg:gap-16 justify-between items-start hero-content' : ' lg:gap-10 justify-start items-start hero-content'}>
+                <div className={displayType ? 'lg:gap-10 justify-between items-start hero-content' : ' lg:gap-10 justify-start items-start hero-content'}>
                     <div className='lg:block hidden leading-9'>
                         <div className='mb-8 w-44'>
                             <h2 className='text-xl font-semibold mb-4'>Category</h2>
@@ -86,68 +89,45 @@ const Products = () => {
 
                     </div>
                     <div className='flex-none w-full'>
+
                         {displayType ? <>
-                            <div className='justify-center grid lg:grid-cols-3 md:grid-cols-2 lg:gap-32 md:gap-6'>
-                                <div className="card lg:w-72 md:w-full min-h-full card-compact bg-base-100 shadow-xl">
-                                    <figure className='w-full h-[250px] mx-auto'><img className='w-full' src='https://i.ibb.co/bsMVr52/Apple-i-Phone-14-Pro-Max.jpg' alt="" /></figure>
-                                    <div className="card-body bg-slate-100 leading-tight">
-                                        <h2 className="card-title text-2xl">Titles</h2>
-                                        <h2 className='text-md font-bold text-md'>Price: $24</h2>
-                                        <p className='font-semibold'>Brand Name: </p>
-                                        <div className="card-actions justify-end">
-                                            <button className="btn btn-primary">Add Cart</button>
+                            <div className='justify-center grid lg:grid-cols-3 md:grid-cols-2 lg:gap-10 md:gap-6 gap-6'>
+                                {
+                                    products.map(product => <>
+                                        <div className="card lg:w-full w-full md:w-full min-h-full card-compact bg-base-100 shadow-xl">
+                                            <figure className='w-full h-[250px] mx-auto'><img className='w-full' src={product.productImage} alt="Shoes" /></figure>
+                                            <div className="card-body bg-slate-100 leading-tight">
+                                                <h2 className="card-title text-2xl">{product.productName}</h2>
+                                                <h2 className='text-md font-bold text-md'>Price: ${product.productPrice}</h2>
+                                                <p className='font-semibold'>Brand Name: {product.brandName}</p>
+                                                <div className="card-actions justify-end">
+                                                    <button className="btn btn-primary">Add Cart</button>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="card lg:w-72 md:w-full min-h-full card-compact bg-base-100 shadow-xl">
-                                    <figure className='w-full h-[250px] mx-auto'><img className='w-full' src='https://i.ibb.co/0jfhH17/sel-43s22kks-01-228x228.webp' alt="Shoes" /></figure>
-                                    <div className="card-body bg-slate-100 leading-tight">
-                                        <h2 className="card-title text-2xl">'Title'</h2>
-                                        <h2 className='text-md font-bold text-md'>Price: $</h2>
-                                        <p className='font-semibold'>Brand Name: </p>
-                                        <div className="card-actions justify-end">
-                                            <button className="btn btn-primary">Add Cart</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="card lg:w-72 md:w-full min-h-full card-compact bg-base-100 shadow-xl">
-                                    <figure className='w-full h-[250px] mx-auto'><img className='w-full' src='https://i.ibb.co/6Dfw7wQ/envy-15-ep1890tx-01-228x228.jpg' alt="Shoes" /></figure>
-                                    <div className="card-body bg-slate-100 leading-tight">
-                                        <h2 className="card-title text-2xl">'Title'</h2>
-                                        <h2 className='text-md font-bold text-md'>Price: $</h2>
-                                        <p className='font-semibold'>Brand Name: </p>
-                                        <div className="card-actions justify-end">
-                                            <button className="btn btn-primary">Add Cart</button>
-                                        </div>
-                                    </div>
-                                </div>
+                                    </>)
+                                }
                             </div>
 
                         </>
                             :
                             <>
-                                <div className="mb-5 card w-full card-side bg-base-100 shadow-xl">
-                                    <figure className='w-72 h-[250px] mx-auto'><img src="https://i.ibb.co/bsMVr52/Apple-i-Phone-14-Pro-Max.jpg" alt="Movie" /></figure>
-                                    <div className="card-body">
-                                        <h2 className="card-title text-2xl">'Title'</h2>
-                                        <h2 className='text-md font-bold text-md'>Price: $</h2>
-                                        <p className='font-semibold'>Brand Name: </p>
-                                        <div className="card-actions justify-end">
-                                            <button className="btn btn-primary">Add Cart</button>
+
+                                {
+                                    products.map(product => <>
+                                        <div className="mb-5 card w-full card-side bg-base-100 shadow-xl">
+                                            <figure className='w-72 h-[250px] mx-auto'><img src={product.productImage} alt="Movie" /></figure>
+                                            <div className="card-body">
+                                                <h2 className="card-title text-2xl">{product.productName}</h2>
+                                                <h2 className='text-md font-bold text-md'>Price: ${product.productPrice}</h2>
+                                                <p className='font-semibold'>Brand Name: {product.brandName}</p>
+                                                <div className="card-actions justify-end items-center">
+                                                    <button className="btn btn-primary">Add Cart</button>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="card w-full card-side bg-base-100 shadow-xl">
-                                    <figure className='w-72 h-[250px] mx-auto'><img src="https://i.ibb.co/bsMVr52/Apple-i-Phone-14-Pro-Max.jpg" alt="Movie" /></figure>
-                                    <div className="card-body">
-                                        <h2 className="card-title text-2xl">'Title'</h2>
-                                        <h2 className='text-md font-bold text-md'>Price: $</h2>
-                                        <p className='font-semibold'>Brand Name: </p>
-                                        <div className="card-actions justify-end">
-                                            <button className="btn btn-primary">Add Cart</button>
-                                        </div>
-                                    </div>
-                                </div>
+                                    </>)
+                                }
                             </>}
                     </div>
                 </div>
